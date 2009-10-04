fceb429b8e1f8c9b4490160394ec1a6fef20f72a
Anil Madhavapeddy <anil@recoil.org>
2009-10-04 15:27:17 +0100

    initial import of personal container server repository skeleton

---
+++ server/config.ml
@@ -0,0 +1,20 @@
+open Schema.Config
+let conf = ref None
+
+let cfn fn = match !conf with 
+          None -> failwith "configuration not initalized"
+        | Some c -> fn c
+
+let rex = Pcre.regexp "\\$HOME"
+let itempl = Pcre.subst (Sys.getenv "HOME")
+let s = Pcre.replace ~rex ~itempl
+
+let init file =
+  conf := Some (c_of_json (Json_io.load_json file))
+
+module Dir = struct
+  let db () = cfn (fun c -> s c.db_directory)
+  let log () = cfn (fun c -> s c.log_directory)
+  let static () = cfn (fun c -> s c.static_directory)
+  let port () = cfn (fun c -> c.port)
+end
---
+++ server/schema.ml
@@ -0,0 +1,47 @@
+(*pp camlp4o `ocamlfind query -i-format type-conv orm.syntax json-static` pa_type_conv.cmo pa_orm.cma pa_json_tc.cmo *)
+
+
+module Config = struct
+  type c = {
+    db_directory: string;
+    log_directory: string;
+    static_directory: string;
+    port: int
+  } with json
+end
+ 
+module Entry = struct
+  type 
+    contact = {
+      first_name: string option;
+      last_name: string option;
+      abrec: string option;
+      uid: string;
+   }
+ and
+   svc = {
+     name: string;
+     path: string;
+     contact: contact option
+   }
+ and
+   meta = {
+     k: string;
+     v: string
+   }
+ and
+   e = {
+     _source: string;
+     _timestamp: float;
+     _uid: string;
+     _from: svc list;
+     _to: svc list;
+     _meta: meta list;
+   }
+ with 
+   json, 
+   orm ( debug: all; 
+         dot: "schema.dot"; 
+         unique: contact<uid>, e<_uid> )
+
+end
