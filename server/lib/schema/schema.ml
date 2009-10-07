14cad6ff44d719f75a9b057dbb3c48caffb520a4
Anil Madhavapeddy <anil@recoil.org>
2009-10-07 17:16:40 +0100

    fixes to schema (dont use reserved words) and add a debugging target to makefile

--- server/lib/schema/schema.ml
+++ server/lib/schema/schema.ml
@@ -14,35 +14,30 @@ end
 module Entry = struct
   type 
     contact = {
-      first_name: string option;
-      last_name: string option;
-      abrec: string option;
-      uid: string;
+      c_origin: string;
+      mutable c_mtime: float;
+      c_uid: string;
+      mutable c_meta: (string * string) list
    }
  and
    svc = {
-     name: string;
-     path: string;
-     contact: contact option
-   }
- and
-   meta = {
-     k: string;
-     v: string
+     s_ty: string;
+     s_id: string;
+     s_co: string option
    }
  and
    e = {
-     _source: string;
-     _timestamp: float;
-     _uid: string;
+     source: string;
+     mtime: float;
+     uid: string;
      _from: svc list;
      _to: svc list;
-     _meta: meta list;
+     meta: (string * string) list;
    }
  with 
    json, 
    orm ( debug: all; 
          dot: "schema.dot"; 
-         unique: contact<uid>, e<_uid> )
+         unique: contact<c_uid>, e<uid>, svc<s_ty,s_id> )
 
 end
