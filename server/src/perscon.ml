26680eb2ad3592eddff6f875fec683cf143eebf4
Anil Madhavapeddy <anil@recoil.org>
2009-10-07 17:28:27 +0100

    make directories at startup

--- server/src/perscon.ml
+++ server/src/perscon.ml
@@ -6,6 +6,22 @@ open Lwt
 open Log
 open Cohttp
 
+module Dirs = struct
+  exception Unable_to_make_dirs of string * string
+  let make dir =
+    let rec fn dir accum = 
+      match dir with
+      |"/"|""|"." -> raise (Unable_to_make_dirs (dir, String.concat "/" accum))
+      |_ when try Sys.is_directory dir with Sys_error _ -> false ->
+        ignore(List.fold_left (fun a b ->
+          let c = Filename.concat a b in
+          Unix.handle_unix_error Unix.mkdir c 0o755;
+          c) dir accum)
+      |_ ->
+        fn (Filename.dirname dir) ((Filename.basename dir) :: accum)
+    in fn dir []
+end
+
 let _ =
   let config_file = ref "perscon.conf" in
   let spec = [
@@ -25,14 +41,17 @@ let _ =
       |None ->
         prerr_endline "Unable to retrieve passphrase for root user";
         exit 1;
-      |Some p -> p
-    in
+      |Some p -> p in
+
     let port = Config.Dir.port () in
     let spec = {  Http_daemon.default_spec with
       Http_daemon.auth = Some ("Personal Container", `Basic ("root", phrase));
       callback = Dispatch.t;
       port = port } in
    
+    logmod "Server" "creating log and db directories";
+   List.iter Dirs.make [ Config.Dir.db () ; Config.Dir.log () ];
+
     logmod "Server" "initializing MIME types";
     Mime.init (Filename.concat (Config.Dir.etc ()) "mime.types") >>
 
