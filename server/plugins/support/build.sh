656282bc3cc9dd2cdf93e23f3bb820855edddc59
Anil Madhavapeddy <anil@recoil.org>
2009-10-09 09:55:14 +0100

    syncservices pyobjc is part of base snow leopard, so dont build it

--- server/plugins/support/build.sh
+++ server/plugins/support/build.sh
@@ -14,14 +14,6 @@ cd simplejson-$V
 mv dist/simplejson-$V-py2.6-macosx-10.6-universal.egg ../..
 cd ..
 
-V=2.2b2
-ftp http://pypi.python.org/packages/source/p/pyobjc-framework-SyncServices/pyobjc-framework-SyncServices-$V.tar.gz
-tar -zxvf pyobjc-framework-SyncServices-$V.tar.gz
-cd pyobjc-framework-SyncServices-$V
-/usr/bin/python setup.py bdist_egg
-mv dist/pyobjc_framework_SyncServices-$V-py2.6.egg ../..
-cd ..
-
 cd ..
 rm -rf $OBJDIR
 
