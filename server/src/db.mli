5a40939ace9e2ac07f96fd504f0ae813f2f3007c
Anil Madhavapeddy <anil@recoil.org>
2009-10-23 16:47:09 +0100

    update interface (still ugly ORM internal) with 0.4 ORM changes

--- server/src/db.mli
+++ server/src/db.mli
@@ -1,11 +1,11 @@
 module TimeDB :
   sig
     val with_db :
-      float -> (Schema.Entry.Orm._cache Schema.Entry.OS.state -> 'a) -> 'a
+      float -> (Schema.Entry.Orm._cache Schema.Entry.Orm.OS.state -> 'a) -> 'a
   end
 
 module SingleDB :
   sig
     val with_db :
-      (Schema.Entry.Orm._cache Schema.Entry.OS.state -> 'a) -> 'a
+      (Schema.Entry.Orm._cache Schema.Entry.Orm.OS.state -> 'a) -> 'a
   end
