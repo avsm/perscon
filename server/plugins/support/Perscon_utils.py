3203742a54821c964c3f27470eac59f4e4cc9d71
Anil Madhavapeddy <anil@recoil.org>
2009-10-25 20:54:51 +0000

    add in an rpc call which takes care of mime-type and escaping for HTTP calls from python plugins

--- server/plugins/support/Perscon_utils.py
+++ server/plugins/support/Perscon_utils.py
@@ -1,7 +1,9 @@
-import urllib2
+import urllib2,urllib
 import commands
 
 script = "../../scripts/get_passphrase.sh"
+
+localuri = None
 
 def get_perscon_password():
     status, passwd = commands.getstatusoutput(script)
@@ -11,6 +13,7 @@ def get_perscon_password():
         return ''
 
 def init_url (uri):
+    global localuri
     passwd = get_perscon_password ()
     ah = urllib2.HTTPBasicAuthHandler()
     ah.add_password(realm='Personal Container',
@@ -19,3 +22,21 @@ def init_url (uri):
                     passwd=passwd)
     op = urllib2.build_opener(ah)
     urllib2.install_opener(op)
+    localuri = uri
+
+def rpc(urifrag, delete=False, args=None, data=None, headers={}):
+    headers['content-type'] = 'application/json'
+    uri = localuri + urllib.quote(urifrag)
+    if args:
+      uri += "?" + urllib.urlencode(args)
+    print "rpc: " + uri
+    if delete:
+      meth="DELETE"
+    else:
+      if data:
+        meth="POST"
+      else:
+        meth="GET"
+    req = urllib2.Request(uri, data=data, headers=headers)
+    req.get_method = lambda: meth
+    return urllib2.urlopen(req)
