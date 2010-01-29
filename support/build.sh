c644c9d77874f938fd6fbc34d0877fffda19a349
Malte Schwarzkopf <ms705@cl.cam.ac.uk>
2010-01-29 16:07:51 +0000

    changed build script to allow running the server on Linux

--- support/build.sh
+++ support/build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash 
 # build eggs used by the plugins
 
+PLATFORM=`uname`
+
 CDIR=$(pwd)
 V=2.0.9
 set -ex
@@ -17,21 +19,30 @@ cd perscon-imap
 mv dist/offlineimap-6.2.0-py2.6.egg $CDIR/
 cd $CDIR
 
-SJEGG=simplejson-$V-py2.6-macosx-10.6-universal.egg
+if [[ $PLATFORM == 'Darwin' ]]; then
+  SJEGG=simplejson-$V-py2.6-macosx-10.6-universal.egg
+elif [[ $PLATFORM == "Linux" ]]; then
+  SJEGG=simplejson-$V-py2.6.egg
+fi
 if [ ! -f "$CDIR/$SJEGG" ]; then
   cd $OBJDIR
-  ftp http://pypi.python.org/packages/source/s/simplejson/simplejson-$V.tar.gz
+  wget http://pypi.python.org/packages/source/s/simplejson/simplejson-$V.tar.gz
   tar -zxvf simplejson-$V.tar.gz
   cd simplejson-$V
   /usr/bin/python setup.py bdist_egg
   mv dist/$SJEGG $CDIR
   cd ..
 fi
+
 SV=0.16.0
-STORM=storm-$SV-py2.6-macosx-10.6-universal.egg
+if [[ $PLATFORM == 'Darwin' ]]; then
+  STORM=storm-$SV-py2.6-macosx-10.6-universal.egg
+elif [[ $PLATFORM == 'Linux' ]]; then
+  STORM=storm-$SV-py2.6-linux-`uname -a`.egg
+fi
 if [ ! -f "$CDIR/$STORM" ]; then
   cd $OBJDIR
-  ftp http://launchpad.net/storm/trunk/0.16/+download/storm-$SV.tar.bz2
+  wget http://launchpad.net/storm/trunk/0.16/+download/storm-$SV.tar.bz2
   tar -jxvf storm-$SV.tar.bz2
   cd storm-$SV
   /usr/bin/python setup.py bdist_egg
