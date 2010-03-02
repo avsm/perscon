57082d52319590e6b42fcc5e26181fa1b380b836
Anil Madhavapeddy <anil@recoil.org>
2010-03-02 11:57:12 +0000

    simple skiplist for duplicate locations

--- appengine/location_csv.py
+++ appengine/location_csv.py
@@ -24,17 +24,29 @@ loader = [
            ('speed', float_or_none),
          ]
 
+seen = {}
+
 def main():
     if len(sys.argv) < 2:
         print >> sys.stderr, "Usage: %s <location.csv>" % sys.argv[0]
         exit(1)
     fin = open(sys.argv[1], 'r')
     r = csv.reader(fin)
+    skip = 0
+    total = 0
     for row in r:
+        total += 1
         e = {}
         for i in range(len(loader)):
             e[loader[i][0]] = loader[i][1](row[i])
-        print e   
+        if e['date'] in seen:
+          skip += 1
+          if seen[e['date']] != e['loc']:
+             print "%s diff dup: %s %s" % ( e['date'], e['loc'], seen[e['date']])
+        else:
+          seen[e['date']] = e['loc']
+#        print e   
+    print skip, total
 
 if __name__ == "__main__":
     main()
