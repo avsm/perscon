e1a44763821a63811da03244c00029a0662932c6
Anil Madhavapeddy <anil@recoil.org>
2010-04-05 04:06:28 +0800

    add skeleton controller

---
+++ ui/apps/perscon/controllers/app.js
@@ -0,0 +1,18 @@
+// ==========================================================================
+// Project:   Perscon.appController
+// Copyright: ©2010 My Company, Inc.
+// ==========================================================================
+/*globals Perscon */
+
+/** @class
+
+  (Document Your Controller Here)
+
+  @extends SC.Object
+*/
+Perscon.appController = SC.ObjectController.create(
+/** @scope Perscon.appController.prototype */ {
+
+  // TODO: Add your own code here.
+
+}) ;
---
+++ ui/apps/perscon/tests/controllers/app.js
@@ -0,0 +1,15 @@
+// ==========================================================================
+// Project:   Perscon.appController Unit Test
+// Copyright: ©2010 My Company, Inc.
+// ==========================================================================
+/*globals Perscon module test ok equals same stop start */
+
+module("Perscon.appController");
+
+// TODO: Replace with real unit test for Perscon.appController
+test("test description", function() {
+  var expected = "test";
+  var result   = "test";
+  equals(result, expected, "test should equal test");
+});
+
