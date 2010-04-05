a6fde330ec4156f9d91b021d2022c5d14a68637a
Anil Madhavapeddy <anil@recoil.org>
2010-04-05 18:54:15 +0800

    add in skeleton settings dialog pane

---
+++ ui/apps/perscon/controllers/prefs.js
@@ -0,0 +1,20 @@
+// ==========================================================================
+// Project:   Perscon.prefsController
+// Copyright: ©2010 My Company, Inc.
+// ==========================================================================
+/*globals Perscon */
+
+/** @class
+
+  (Document Your Controller Here)
+
+  @extends SC.Object
+*/
+Perscon.prefsController = SC.ObjectController.create(
+/** @scope Perscon.prefsController.prototype */ {
+
+  email: 'abc@def.com',
+  firstName: 'Aaa',
+  lastName: 'Bbb',
+
+}) ;
---
+++ ui/apps/perscon/tests/models/prefs.js
@@ -0,0 +1,15 @@
+// ==========================================================================
+// Project:   Perscon.Prefs Unit Test
+// Copyright: ©2010 My Company, Inc.
+// ==========================================================================
+/*globals Perscon module test ok equals same stop start */
+
+module("Perscon.Prefs");
+
+// TODO: Replace with real unit test for Prefs
+test("test description", function() {
+  var expected = "test";
+  var result   = "test";
+  equals(result, expected, "test should equal test");
+});
+
