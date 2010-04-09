61b0b0681e077cf112627123bb57591236b72c6c
Anil Madhavapeddy <anil@recoil.org>
2010-04-09 13:33:13 +0100

    fix person POST

--- appengine/views.py
+++ appengine/views.py
@@ -37,7 +37,6 @@ from models import Location,Att,Person,Message
 
 def message(request, uid):
     meth = request.method
-    logging.info
     if meth == 'POST':
         j = json.loads(request.raw_post_data)
         created = datetime.fromtimestamp(float(j['mtime']))
@@ -87,7 +86,7 @@ def person(request, uid):
     if meth == 'POST':
         j = json.loads(request.raw_post_data)
         created = datetime.fromtimestamp(float(j['mtime']))
-        services = map(lambda x: db.IM(x[0].lowercase(), address=x[1].lowercase()), j['services'])
+        services = map(lambda x: db.IM(x[0].lower(), address=x[1].lower()), j['services'])
         atts = filter(None, map(lambda x: Att.get_by_key_name(x), j['atts']))
         logging.info(atts)
         atts = map(lambda x: x.key(), atts)
