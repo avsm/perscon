5133d69711a0a7e583be94587e40b41c3c21e03e
Anil Madhavapeddy <anil@recoil.org>
2010-04-20 19:36:59 +0100

    bug fixes to the dispatcher in the ocaml server, to parse locations now

--- server/src/dispatch.ml
+++ server/src/dispatch.ml
@@ -27,7 +27,8 @@ module Resp = struct
     logmod "HTTP" "Debug response: %s" body;
     let headers = [ "Cache-control", "no-cache"; "Mime-type", "text/plain" ] in
     let status = `Status (`Success `OK) in
-    return (Http_response.init ~body:[`String body] ~headers ~status ())
+    let body = [`String body] in
+    return (Http_response.init ~body ~headers ~status ())
 
   (* blank ok for RPC successes *)
   let ok req =
@@ -65,12 +66,36 @@ module Resp = struct
       | `DELETE ->      ofn0 args delete
 
   let handle_json req fn =
-    try
-      fn ()
-    with
-    | e ->
-        let err = Printexc.to_string e in
-        bad_args ~err ("Json error: " ^ Printexc.get_backtrace ())
+    try  fn ()
+    with e ->
+      let err = Printexc.to_string e in
+      bad_args ~err ("Json error: " ^ Printexc.get_backtrace ())
+end
+
+module Person = struct
+
+  let crud req args =
+    let get = function
+       |[uid] -> Resp.debug req "foo"
+       | _ -> Resp.not_found req "not found"
+    in
+    Resp.crud ~get req args 
+end
+
+let safe_try fn arg =
+  try return (fn arg)
+  with e -> fail e
+
+module Loc = struct
+  let crud req args =
+    let post doc = function
+      | [] -> 
+          lwt body = Http_message.string_of_body doc in
+          lwt j = try_lwt return (Schema.location_of_json body) with e -> fail e in
+          Resp.ok req
+      | _ -> Resp.not_found req "unknown post"
+  in
+  Resp.crud ~post req args    
 end
 
 (* dispatch HTTP requests *)
@@ -83,15 +108,19 @@ let dispatch req oc =
     logmod "HTTP" "Serving static file: %s (%s)" fname mime_type;
     Http_daemon.respond_file ~fname ~droot:("todo") ~mime_type oc in
   function
-  | [] :: "static" :: tl -> (* static *)
-     let path = String.concat "/" tl in
-     let mime_type = Magic_mime.lookup path in
-     let fname = Filename.concat (Config.Dir.static ()) path in
-     logmod "HTTP" "serving file: %s (%s)" fname mime_type;
-     static fname mime_type
-  | [] :: args -> (* dynamic *)
-     match args with
-       | _ -> dyn Resp.not_found "unknown url"
+  | "" :: "static" :: tl -> (* static *)
+      let path = String.concat "/" tl in
+      let mime_type = Magic_mime.lookup path in
+      let fname = Filename.concat (Config.Dir.static ()) path in
+      logmod "HTTP" "serving file: %s (%s)" fname mime_type;
+      static fname mime_type
+  | "" :: args -> begin (* dynamic *)
+      match args with
+        | "person" :: x -> dyn Person.crud x
+        | "loc" :: x -> dyn Loc.crud x
+        | _ -> dyn Resp.not_found "unknown url"
+  end
+  | _ -> dyn Resp.not_found "unknown url"
  
 (* main callback function *)
 let t req oc =
@@ -104,4 +133,11 @@ let t req oc =
   let split_path = Pcre.split ~pat:"/" path in
   let normalized_path = List.filter (function |"."|".." -> false |_ -> true) split_path in
   logmod "HTTP" "normalized_path: %d" (List.length normalized_path);
-  dispatch req oc normalized_path
+  try_lwt
+      dispatch req oc normalized_path
+  with e -> begin
+      print_endline "exception:";
+      print_endline (Printexc.to_string e);
+      lwt resp = Resp.bad_args "internal server error" in
+      Http_daemon.respond_with resp oc
+  end
