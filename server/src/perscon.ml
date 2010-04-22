7e6dfc9fbb2cd66a9d643aaaf12b4da65d264951
Anil Madhavapeddy <anil@recoil.org>
2010-04-23 00:33:29 +0800

    make directories at startup for db, logs

--- server/src/perscon.ml
+++ server/src/perscon.ml
@@ -4,6 +4,22 @@ open Lwt
 open Log
 open Cohttpserver
 
+module Dirs = struct
+  exception Unable_to_make_dirs of string * string
+  let make dir =
+    let rec fn dir accum = 
+      match dir with
+      |"/"|""|"." -> raise (Unable_to_make_dirs (dir, String.concat "/" accum))
+      |_ when try Sys.is_directory dir with Sys_error _ -> false ->
+        ignore(List.fold_left (fun a b ->
+          let c = Filename.concat a b in
+          Unix.handle_unix_error Unix.mkdir c 0o755;
+          c) dir accum)
+      |_ ->
+        fn (Filename.dirname dir) ((Filename.basename dir) :: accum)
+    in fn dir []
+end
+
 let _ = Sys.set_signal Sys.sigpipe Sys.Signal_ignore
 
 let _ =
@@ -22,6 +38,7 @@ let _ =
       Http_daemon.auth = `None;
       callback = Dispatch.t;
       port = http_port } in
+    List.iter Dirs.make [ Config.Dir.db () ; Config.Dir.log () ];
     let http =
       logmod "Server" "listening to HTTP on port %d" http_port;
       Http_daemon.main http_spec in
